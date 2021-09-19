1477365b726046b0e9b72abffc16a62f801db083
santergameboy <santergameboy@gmail.com>
2021-09-19 19:57:27 -0400

    ToolBar

---
+++ app/src/main/java/com/example/infgra/LoginActivity.java
@@ -0,0 +1,20 @@
+package com.example.infgra;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+    public void goCreateAccount(View view){
+
+        System.out.println("-----------------> go create Account");
+
+    }
+}
